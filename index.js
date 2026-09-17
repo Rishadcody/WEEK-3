74af1b8cfbdda73a87c2d7f95f3733c1c63b70cb
Rishadcody <baburishad664@gmail.com>
2026-09-17 12:16:08 +0530

    js practice

---
+++ index.js
@@ -0,0 +1,142 @@
+//  let fruits = ["Apple", "Mango"];
+
+//  fruits.push("Orange", "Banana");
+
+// console.log(fruits);
+
+
+
+
+
+
+//  let fruits = ["Apple", "Mango", "Banana"];
+
+//  fruits.pop();
+
+// console.log(fruits);
+
+
+
+
+// let fruits = ["Mango", "Banana"];
+
+// fruits.unshift("Apple");
+
+// console.log(fruits);
+
+
+
+
+// let fruits = ["Apple", "Mango", "Banana"];
+
+// fruits.shift();
+
+// console.log(fruits);
+
+
+
+
+
+// let fruits =["Apple", "Mango", "Banana"]
+
+//  fruits.splice(1,1,"orange")
+
+// console.log(fruits)
+
+
+
+
+// let str = "hello world";
+
+// console.log(str.toUpperCase());
+
+
+
+// let str = "JAVASCRIPT";
+
+// console.log(str.toLowerCase());
+
+
+
+
+// let str = "Programming";
+
+// console.log(str.length);
+
+
+
+// let str = "Hello World"
+// console.log(str.replace("World","Javascript"))
+
+
+
+
+// let str = "JavaScript is awesome";
+
+// console.log(str.includes("awesome"));
+
+
+
+
+// let person={
+
+//   name: "john",
+// }
+// let details ={
+
+//     age : 25,
+//   city: "kochi"
+// }
+// let result= Object.assign(person,details)
+// console.log(result)
+
+
+
+
+
+// let person = {
+//   name: "John",
+//   age: 25,
+//   city: "Kochi"
+// };
+
+// let result= (Object.keys(person));
+// console.log(result)
+
+
+
+
+// let person = {
+//   name: "John",
+//   age: 25,
+//   city: "Kochi"
+// };
+
+// let result= (Object.values(person));
+// console.log(result)
+
+
+
+
+// let person = {
+//   name: "John",
+//   age: 25
+// };
+
+// let result=(Object.entries(person));
+// console.log(result)
+
+
+
+
+// let person=[
+//   ["name","john"],
+//   ["age",25],
+//   ["city","kochi"]
+// ];
+// let result= Object.fromEntries(person)
+// console.log(result)
+
+
+
+
